3b3e794cab500412c75865f13f2c5d069faaed5b
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2023-03-17 13:06:20 -0700

    Create Button_Lights.ino

---
+++ Button_Lights.ino
@@ -0,0 +1,43 @@
+//set vars to led pin outs
+const int red = 2;
+const int yellow = 3;
+const int green = 4;
+const int white = 5;
+
+//set var to button pin in
+const int button = 13;
+
+void setup() {
+
+  //set led pins to output
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(white, OUTPUT);
+
+  //set button pin to input
+  pinMode(button, INPUT);
+  
+}
+void loop() {
+  
+  //if button not pressed?
+  if (digitalRead(button) == LOW) {
+
+    //turn on all LEDs
+    digitalWrite(red, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(green, LOW);
+    digitalWrite(white, LOW);
+    
+  //if button is pressed?
+  } else if (digitalRead(button) == HIGH) {
+
+    //turn off all LEDs
+    digitalWrite(red, HIGH);
+    digitalWrite(yellow, HIGH);
+    digitalWrite(green, HIGH);
+    digitalWrite(white, HIGH);
+    
+  }
+}
